e9cc1c9e92b909a2c5e8d5320296c205a8dcd785
veerasekharad <75124207+veerasekharad@users.noreply.github.com>
2021-10-03 18:28:37 +0530

    Add files via upload

--- run/Run_Report_Covid19_Vaccinated_12_18.sh
+++ run/Run_Report_Covid19_Vaccinated_12_18.sh
@@ -1,1 +1,7 @@
- 
+spark-submit \
+   --master yarn \
+   --deploy-mode client \
+   --packages com.crealytics:spark-excel_2.11:0.11.1 \
+   spark/COVID_19_Vaccinations_Report.py \
+   config/COVID_19_Vaccination.conf
+   
